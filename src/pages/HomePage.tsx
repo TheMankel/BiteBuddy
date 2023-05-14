568bd9f2cccaf17e85d7732d3af0b29775f7c94f
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-05-14 23:59:26 +0200

    Created HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Home from '../components/Home/Home';
+import AppLayout from '../layout/AppLayout';
+
+const HomePage = () => {
+  return (
+    <AppLayout>
+      <Home />
+    </AppLayout>
+  );
+};
+
+export default HomePage;
