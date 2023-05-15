8d7d280f884629e0b9e19d9f5e305f74efef4fa5
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-05-15 12:09:31 +0200

    Changed the look of the Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,26 @@
 import { Paper, Typography } from '@mui/material';
+import ProductsForm from './ProductsForm/ProductsForm';
 
 const Home = () => {
   return (
-    <Paper
-      variant='outlined'
-      sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-      <Typography component='h1' variant='h4' align='center' gutterBottom>
+    <Paper elevation={3} sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+      <Typography
+        component='h1'
+        variant='h4'
+        align='center'
+        fontWeight={700}
+        color='text.primary'>
         Bite Buddy
       </Typography>
-      Here will be form component
+      <Typography
+        component='p'
+        variant='h5'
+        align='center'
+        color='text.secondary'
+        gutterBottom>
+        Fill out the form to place your order
+      </Typography>
+      <ProductsForm />
     </Paper>
   );
 };
