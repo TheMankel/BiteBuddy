df2a3429395e1b57d2741890d49363265e958ed6
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-05-15 12:01:34 +0200

    Created ProductType

---
+++ src/types/ProductType.ts
@@ -0,0 +1,10 @@
+export type ProductType = {
+  id?: string;
+  name: string;
+  preparation_time: string;
+  type: string;
+  diameter?: number;
+  no_of_slices?: number;
+  spiciness_scale?: number;
+  slices_of_bread?: number;
+};
