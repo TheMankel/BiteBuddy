07f823974a25adda8d95ea3d93e02b0bf56ed388
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-05-15 17:26:30 +0200

    Created usePostData hook

---
+++ src/components/Home/ProductsForm/hooks/usePostData.ts
@@ -0,0 +1,31 @@
+import { ProductType } from '../../../../types/ProductType';
+
+const usePostData = () => {
+  const postData = async (
+    productsData: ProductType,
+    url: string,
+    headers?: object,
+  ) => {
+    return await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+      body: JSON.stringify(productsData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+        return err.message;
+      });
+  };
+
+  return postData;
+};
+
+export default usePostData;
